fecd1c602c6e9b65d7ebe723fcec246196bdef5d
victor <victor.hugo@eossystems.com.br>
2023-07-31 14:23:16 -0400

    refactor: icons folder created

--- capputeeno/src/components/cart-control.tsx
+++ capputeeno/src/components/cart-control.tsx
@@ -1,5 +1,5 @@
 import { useLocalStorage } from "@/hooks/useLocalStorage";
-import { CartIcon } from "./cart-icon";
+import { CartIcon } from "./icons/cart-icon";
 import { styled } from "styled-components";
 
 const CartCount = styled.span`
@@ -18,7 +18,7 @@ const Container = styled.div`
 `
 
 export function CartControl() {
-  const { value } = useLocalStorage('cart-items')
+  const { value } = useLocalStorage('cart-items', [])
 
   return (
     <Container>
--- capputeeno/src/components/filter-by-priority.tsx
+++ capputeeno/src/components/filter-by-priority.tsx
@@ -1,5 +1,5 @@
-import styled from "styled-components"
-import { ArrowIcon } from "./arrow-icon"
+import { styled } from "styled-components"
+import { ArrowIcon } from "./icons/arrow-icon"
 import { useState } from "react"
 import { useFilter } from "@/hooks/useFilter"
 import { PriorityTypes } from "@/types/priority-types"
@@ -10,47 +10,49 @@ const FilterContainer = styled.div`
   position: relative;
 
   button {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: none;
-    background: transparent;
-    cursor: pointer;
+      border: none;
+      background: transparent;
+      cursor: pointer;
 
-    font-family: inherit;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 22px;
-    color: (--text-dark);
+      font-family: inherit;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 22px;
+      color: var(--text-dark);
 
-    svg{
-      margin-left: 16px;
-    }
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+          margin-left: 16px;
+      }
   }
 `
 
 const PriorityFilter = styled.ul`
-  position: relative;
-  background: #FFF;
+  position: absolute;
   width: 250px;
+  background: #FFF;
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
   border-radius: 4px;
   padding: 12px 16px;
   z-index: 999;
 
   list-style: none;
+
   top: 100%;
 
-  li{
-    color: var(--text-dark);
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 22px;
-    cursor: pointer;
+  li {
+      color: var(--text-dark);
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 22px;
+      cursor: pointer;
   }
 
-  li + li{
-    margin-top: 4px;
+  li + li {
+      margin-top: 4px;
   }
 `
 
@@ -71,7 +73,6 @@ export function FilterByPriority() {
         Organizar por
         <ArrowIcon />
       </button>
-
       {isOpen &&
         <PriorityFilter>
           <li onClick={() => handleUpdatePriority(PriorityTypes.NEWS)}>Novidades</li>
